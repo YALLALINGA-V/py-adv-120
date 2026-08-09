bccde2a4b8a6c109f5efb00b25cdc31b8604fce0
YALLALINGA.V <yallalinga0408@gmail.com>
2026-08-09 07:31:55 +0530

    Time: 3 ms (21.27%) | Memory: 19.5 MB (53.10%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -2,13 +2,23 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         li=[]
         for i in range(1,n+1):
-            if i%3!=0 and i%5 !=0:
-                li.append(str(i))
             if i%3==0 and i%5==0:
                 li.append("FizzBuzz")
-                continue
-            if i%3==0:
+            elif i%3==0:
                 li.append("Fizz")
-            if i%5==0:
+            elif i%5==0:
                 li.append("Buzz")
+            else:
+                li.append(str(i))
         return li
+        # for i in range(1,n+1):
+        #     if i%3!=0 and i%5 !=0:
+        #         li.append(str(i))
+        #     if i%3==0 and i%5==0:
+        #         li.append("FizzBuzz")
+        #         continue
+        #     if i%3==0:
+        #         li.append("Fizz")
+        #     if i%5==0:
+        #         li.append("Buzz")
+        # return li
