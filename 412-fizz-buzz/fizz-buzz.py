352e11c8e22fce6cf3df019051061e1e5a795824
YALLALINGA.V <yallalinga0408@gmail.com>
2026-08-09 07:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (16.62%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -11,14 +11,3 @@ class Solution:
             else:
                 li.append(str(i))
         return li
-        # for i in range(1,n+1):
-        #     if i%3!=0 and i%5 !=0:
-        #         li.append(str(i))
-        #     if i%3==0 and i%5==0:
-        #         li.append("FizzBuzz")
-        #         continue
-        #     if i%3==0:
-        #         li.append("Fizz")
-        #     if i%5==0:
-        #         li.append("Buzz")
-        # return li
